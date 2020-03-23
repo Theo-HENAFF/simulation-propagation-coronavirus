3bdaaf06a6b1e3879253554001de11892342269b
Theo-HENAFF <theo.henaff@hotmail.fr>
2020-03-23 09:34:14 +0100

    minor changes

--- main.py
+++ main.py
@@ -5,29 +5,39 @@ class Person(object):
     def __init__(self, env, contagious_time, mortality_transmission_rate, vaccine_efficiency):
         self.env = env
         self.action = env.process(self.run())
+
         self.contagious_time = contagious_time
         self.mortality_transmission_rate = mortality_transmission_rate
         self.vaccine_efficiency = vaccine_efficiency
 
+    def run(self):
+        while True:
+            print('Start parking and charging at %d' % self.env.now)
+            charge_duration = 5
+            # We may get interrupted while charging the battery
+            try:
+                yield self.env.process(self.charge(charge_duration))
+            except simpy.Interrupt:
+                # When we received an interrupt, we stop charging and
+                # switch to the "driving" state
+                print('Was interrupted. Hope, the battery is full enough ')
 
-def run(self):
-    while True:
-        print('Start parking and charging at %d' % self.env.now)
-        charge_duration = 5
-        # We may get interrupted while charging the battery
-        try:
-            yield self.env.process(self.charge(charge_duration))
-        except simpy.Interrupt:
-            # When we received an interrupt, we stop charging and
-            # switch to the "driving" state
-            print('Was interrupted. Hope, the battery is full enough ')
+            print('Start driving at %d' % self.env.now)
+            trip_duration = 2
+            yield self.env.timeout(trip_duration)
 
-        print('Start driving at %d' % self.env.now)
-        trip_duration = 2
-        yield self.env.timeout(trip_duration)
-
-
-def charge(self, duration):
-    yield self.env.timeout(duration)
+    def charge(self, duration):
+        yield self.env.timeout(duration)
 
 
+env = simpy.Environment()
+person = Person(env, None, None, None)
+
+
+def driver(env, car):
+    yield env.timeout(3)
+    person.action.interrupt()
+
+
+env.process(driver(env, person))
+env.run(until=15) #Run pdt 15
