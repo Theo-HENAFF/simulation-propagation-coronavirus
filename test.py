84340d4e901c841313387b3bd91e72958b081ad6
Theo-HENAFF <theo.henaff@hotmail.fr>
2020-03-23 11:20:00 +0100

    improvement

--- test.py
+++ test.py
@@ -3,7 +3,34 @@
 import simpy
 
 
-def car(env, name, bcs, driving_time, charge_duration):
+class Person(object):
+    def __init__(self, env,
+                 id,
+                 contagious_time=None,
+                 mortality_transmission_rate=None,
+                 vaccine_efficiency=None,
+                 is_contaminated=False):
+
+        self.env = env
+        self.action = env.process(self.run())
+
+        self.contagious_time = contagious_time
+        self.mortality_transmission_rate = mortality_transmission_rate
+        self.vaccine_efficiency = vaccine_efficiency  # 0 for no vaccine, 1 for total immunity after vaccination
+
+        self.id = id
+        self.health_status = is_contaminated  # healthful/contaminated/dead
+
+
+liste_pers = []
+
+
+def initialisation(nbre_pers):
+    for person in range(nbre_pers):
+        liste_pers.append(Person(env, id=person))
+
+
+def vie(env, name, person, driving_time, charge_duration):
     # Simulate driving to the BCS
     yield env.timeout(driving_time)
 
