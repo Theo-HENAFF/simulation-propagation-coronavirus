c55bb2e1d7f4d4355d30fab17b998bb38b43ab3e
NASRIchiheb <chiheb.nasri23@gmail.com>
2020-03-24 10:32:26 +0100

    Merge branch 'master' of C:\Users\chiheb\Desktop\Info833\EpidemicBroadcast with conflicts.

--- test.py
+++ test.py
@@ -3,20 +3,14 @@
 import simpy
 import random as r
 
-def decision(probability):
-    if r.random() < probability:
-        return True
-    else :
-        return False
-
 class Person(object):
     def __init__(self, env,
-                 id_person,
+                 id,
                  contagious_time=None,
                  mortality_transmission_rate=None,
                  vaccine_efficiency=None,
                  health_status='healthful',
-                 liste_neighbour=[]):
+                 liste_neighbour = []):
 
         # self.env = env
         # self.action = env.process(self.run())
@@ -25,14 +19,14 @@ class Person(object):
         self.mortality_transmission_rate = mortality_transmission_rate
         self.vaccine_efficiency = vaccine_efficiency  # 0 for no vaccine, 1 for total immunity after vaccination
 
-        self.id_person = id_person
+        self.id = id
         self.health_status = health_status  # healthful/cont_without_s/contaminated/dead
         self.cured = False
         self.liste_neighbour = liste_neighbour
 
 nbre_pers = 100
 Xmax = 10  # Nbre max de personnes qu'un individu peut fréquenter
-proba_contamination = 0.25
+proba_contamination = 0.01
 liste_pers = []
 
 
@@ -46,7 +40,7 @@ def initialisation(nbre_pers):
             liste_neighbour.append(n)
 
         # Déclaration des personnes
-        liste_pers.append(Person(env, id_person=person, liste_neighbour=liste_neighbour))
+        liste_pers.append(Person(env, id=person, liste_neighbour=liste_neighbour))
 
     # random conta 1 pers
     id_conta = r.randint(0, nbre_pers)
@@ -54,25 +48,14 @@ def initialisation(nbre_pers):
     print('la personne {} doit arreter de manger de la soupe de chauve souris'.format(id_conta))
 
 
-def vie(env, name, person, driving_time, charge_duration):
-    # Une personne voit avec une probabilité forte son entrourage (ses voisins, ses collègues de travail)
-    proba_meet = 0.75
-    for neighbour in person.liste_neighbour:
-        if decision(proba_meet):
-            print('{} va voir {}'.format(person.id_person, neighbour.id_person))
-            with meeting_point.request() as req:
-                if decision(proba_contamination):
-                    print('Terrrrriiiible {} get coroned'.format(neighbour.id_person))
-                    neighbour.health_status = "cont_without_s"
-                yield req
 
-
-
-
+def vie(env, name, person, driving_time, charge_duration):  
+    # Simulate driving to the BCS
+    yield env.timeout(driving_time)
 
     # Request one of its charging spots
     print('{} arriving at {}'.format(name, env.now))
-    with meeting_point.request() as req:
+    with bcs.request() as req:
         yield req
 
         # Charge the battery
@@ -82,10 +65,10 @@ def vie(env, name, person, driving_time, charge_duration):
 
 
 env = simpy.Environment()
-meeting_point = simpy.Resource(env, capacity=2)  # Seulement 2 personnes peuvent se rencontrer
+bcs = simpy.Resource(env, capacity=2)  # Seulement 2 personnes peuvent se rencontrer
 
 for person in range(len(liste_pers)):
-    env.process(vie(env, 'Car {}'.format(person), meeting_point, person * 2, 5))
+    env.process(vie(env, 'Car {}'.format(person), bcs, person * 2, 5))
 env.run()
 
 
